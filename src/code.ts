35e242d652418c230cd32e4b99670929992f4d05
Roman Nebel <im.roman.nebel@gmail.com>
2024-05-27 22:15:59 +0200

    Update plugin

--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,39 @@
 figma.showUI(__html__, {
   width: 680,
-  height: 600,
+  height: 780,
 });
 
-figma.ui.onmessage = async (msg: { type: string; count: number }) => {
+figma.ui.onmessage = async (msg: { type: string; image: string }) => {
   if (msg.type === "catch") {
     const frame = figma.currentPage.selection[0];
 
     if (frame) {
       const svg = await frame.exportAsync({ format: "SVG_STRING" });
-      figma.ui.postMessage(svg);
+      figma.ui.postMessage({
+        svg,
+        params: {
+          width: frame.width,
+          height: frame.height,
+        },
+      });
     }
+  }
+  if (msg.type === "insert") {
+    figma.createImageAsync(msg.image).then(async (image: Image) => {
+      // Create a rectangle that's the same dimensions as the image.
+      const node = figma.createRectangle();
+
+      const { width, height } = await image.getSizeAsync();
+      node.resize(width, height);
+
+      // Render the image by filling the rectangle.
+      node.fills = [
+        {
+          type: "IMAGE",
+          imageHash: image.hash,
+          scaleMode: "FILL",
+        },
+      ];
+    });
   }
 };
